a75805418c7615dca3080db05ace29f1b3f58dd7
unknown <Abhishek@urvipc.focus.local>
2021-02-22 13:05:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mymob_horoscope/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.mymob_horoscope;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.text.format.Formatter;
+import android.util.Log;
+import android.view.View;
+
+import com.example.mymob_horoscope.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.btnSubmit.setOnClickListener(v -> {
+            WifiManager manager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+            String ip = Formatter.formatIpAddress(manager.getConnectionInfo().getIpAddress());
+            binding.tvIp.setText(ip);
+            Log.e("---IP Address---", "ll_Country IP: " + ip);
+        });
+
+    }
+}
